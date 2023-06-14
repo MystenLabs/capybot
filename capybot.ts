a57e00576abcf5d59da2b3d772d6da3c98501c5b
“George <george.digkas@mystenlabs.com>
2023-06-14 18:04:04 +0300

    wip

--- capybot.ts
+++ capybot.ts
@@ -6,12 +6,16 @@ import {
   TransactionBlock,
 } from "@mysten/sui.js";
 import { setTimeout } from "timers/promises";
-import { defaultAmount } from "./coins/coins";
+import { coins, defaultAmount } from "./coins/coins";
 import { CetusParams, SuiswapParams, TurbosParams } from "./dexs/dexsParams";
 import { Pool } from "./dexs/pool";
 import { logger } from "./logger";
 import { DataEntry } from "./strategies/data_entry";
 import { Strategy } from "./strategies/strategy";
+import {
+  getBalancesForCoinTypes,
+  getTotalBalanceByCoinType,
+} from "./utils/utils";
 
 /**
  * A simple trading bot which subscribes to a number of trading pools across different DEXs. The bot may use multiple
@@ -41,6 +45,21 @@ export class Capybot {
     let startTime = new Date().getTime();
 
     while (new Date().getTime() - startTime < duration) {
+      let coinsBalances = await getBalancesForCoinTypes(
+        this.provider,
+        this.keypair.getPublicKey().toSuiAddress(),
+        new Set<string>([
+          coins.SUI,
+          coins.USDC,
+          coins.CETUS,
+          coins.WETH,
+          coins.USDT,
+          coins.SUIP,
+          coins.SUIA,
+          coins.WSOL,
+        ])
+      );
+
       for (const address in this.pools) {
         let pool = this.pools[address];
         let price = await pool.estimatePrice();
@@ -69,49 +88,105 @@ export class Capybot {
 
           // Execute any suggested trades
           for (const tradeSuggestion of tradeSuggestions) {
-            logger.info({ strategy: strategy.name, decision: tradeSuggestion });
+            // logger.info({ strategy: strategy.name, decision: tradeSuggestion });
             let amountIn: number =
               tradeSuggestion.amount *
               defaultAmount[
                 tradeSuggestion.a2b ? pool.coinTypeB : pool.coinTypeA
               ];
             let amountOut: number = tradeSuggestion.estimatedPrice * amountIn;
-            // TODO: Do these as a programmable transaction
 
             const byAmountIn: boolean = true;
             const slippage: number = 5; // Allow for 5% slippage (??)
 
-            transactionBlock = await this.pools[
-              tradeSuggestion.pool
-            ].createSwapTransaction({
-              transactionBlock,
-              amountIn,
-              amountOut,
-              byAmountIn,
-              slippage,
-            });
+            // const totalCoinTypeBalance = await getTotalBalanceByCoinType(
+            //   this.provider,
+            //   this.keypair.getPublicKey().toSuiAddress(),
+            //   tradeSuggestion.a2b ? pool.coinTypeB : pool.coinTypeA
+            // );
 
-            await this.signer
-              .signAndExecuteTransactionBlock({
-                transactionBlock: transactionBlock,
-                requestType: "WaitForLocalExecution",
-                options: {
-                  showObjectChanges: true,
-                  showEffects: true,
-                },
-              })
-              .then((result) => {
-                // TODO: Execute transaction
-                logger.info(
-                  { strategy: strategy, transaction: result },
-                  "transaction"
-                );
-              })
-              .catch((e) => {
-                logger.error(e);
+            // const totalCoinTypeBalance = await getTotalBalanceByCoinType(
+            //   this.provider,
+            //   this.keypair.getPublicKey().toSuiAddress(),
+            //   this.pools[tradeSuggestion.pool].a2b
+            //     ? pool.coinTypeA
+            //     : pool.coinTypeB
+            // );
+
+            // console.log(
+            //   `**** amountIn: (${amountIn}) totalCoinTypeBalance: ${Number(
+            //     totalCoinTypeBalance
+            //   )}, coin: ${
+            //     this.pools[tradeSuggestion.pool].a2b
+            //       ? pool.coinTypeA
+            //       : pool.coinTypeB
+            //   }`
+            // );
+
+            const totalCoinTypeBalance = coinsBalances.get(
+              this.pools[tradeSuggestion.pool].a2b
+                ? pool.coinTypeA
+                : pool.coinTypeB
+            );
+
+            if (amountIn <= Number(totalCoinTypeBalance)) {
+              console.log(
+                `if (${amountIn} < ${Number(totalCoinTypeBalance)}) - ${
+                  this.pools[tradeSuggestion.pool].a2b
+                    ? pool.coinTypeA
+                    : pool.coinTypeB
+                }, pool: ${this.pools[tradeSuggestion.pool].pool}`
+              );
+              transactionBlock = await this.pools[
+                tradeSuggestion.pool
+              ].createSwapTransaction({
+                transactionBlock,
+                amountIn,
+                amountOut,
+                byAmountIn,
+                slippage,
               });
 
-            transactionBlock = new TransactionBlock();
+              await this.signer
+                .signAndExecuteTransactionBlock({
+                  transactionBlock: transactionBlock,
+                  requestType: "WaitForLocalExecution",
+                  options: {
+                    showObjectChanges: true,
+                    showEffects: true,
+                  },
+                })
+                .then((result) => {
+                  // TODO: Execute transaction
+                  logger.info(
+                    { strategy: strategy, transaction: result },
+                    "transaction"
+                  );
+                })
+                .catch((e) => {
+                  logger.error(e);
+                });
+
+              coinsBalances = await getBalancesForCoinTypes(
+                this.provider,
+                this.keypair.getPublicKey().toSuiAddress(),
+                new Set<string>([
+                  coins.SUI,
+                  coins.USDC,
+                  coins.CETUS,
+                  coins.WETH,
+                  coins.USDT,
+                  coins.SUIP,
+                  coins.SUIA,
+                  coins.WSOL,
+                ])
+              );
+              transactionBlock = new TransactionBlock();
+            } else {
+              console.log(
+                `else (${amountIn} > ${Number(totalCoinTypeBalance)})`
+              );
+            }
           }
         }
       }
