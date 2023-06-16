a38a49f47ed7b37a89205170ccfccd5dd073b935
“George <george.digkas@mystenlabs.com>
2023-06-16 13:22:05 +0300

    fetch from main root files

--- capybot.ts
+++ capybot.ts
@@ -1,165 +1,93 @@
-import {
-  Connection,
-  JsonRpcProvider,
-  Keypair,
-  RawSigner,
-  TransactionBlock,
-} from "@mysten/sui.js";
-import { setTimeout } from "timers/promises";
-import { coins, defaultAmount } from "./coins/coins";
-import { CetusParams, SuiswapParams, TurbosParams } from "./dexs/dexsParams";
-import { Pool } from "./dexs/pool";
-import { logger } from "./logger";
-import { DataEntry } from "./strategies/data_entry";
-import { Strategy } from "./strategies/strategy";
-import { getBalancesForCoinTypes } from "./utils/utils";
+import {Keypair} from "@mysten/sui.js";
+import {Strategy} from "./strategies/strategy";
+import {Pool} from "./dexs/pool";
+import {logger} from "./logger";
+import {setTimeout} from "timers/promises";
+import {DataSource} from "./dexs/data_source";
 
 /**
  * A simple trading bot which subscribes to a number of trading pools across different DEXs. The bot may use multiple
  * strategies to trade on these pools.
  */
 export class Capybot {
-  public pools: Record<
-    string,
-    Pool<CetusParams | SuiswapParams | TurbosParams>
-  > = {};
-  public strategies: Record<string, Array<Strategy>> = {};
-  private keypair: Keypair;
-  private provider: JsonRpcProvider;
-  private signer: RawSigner;
+    public dataSources: Record<string, DataSource> = {};
+    public pools: Record<string, Pool> = {};
+    public strategies: Record<string, Array<Strategy>> = {}
+    private keypair: Keypair;
 
-  constructor(keypair: Keypair) {
-    this.keypair = keypair;
-    this.provider = new JsonRpcProvider(
-      new Connection({
-        fullnode: "https://rpc.mainnet.sui.io:443",
-      })
-    );
-    this.signer = new RawSigner(this.keypair, this.provider);
-  }
+    constructor(keypair: Keypair) {
+        this.keypair = keypair;
+    }
 
-  async loop(duration: number, delay: number) {
-    let startTime = new Date().getTime();
-    const coinsSet = new Set<string>([
-      coins.SUI,
-      coins.USDC,
-      coins.CETUS,
-      coins.WETH,
-      coins.USDT,
-      coins.SUIP,
-      coins.SUIA,
-      coins.WSOL,
-    ]);
+    async loop(duration: number, delay: number) {
+        let startTime = new Date().getTime();
 
-    while (new Date().getTime() - startTime < duration) {
-      // Retrieve the balances for the coins
-      let coinsBalances = await getBalancesForCoinTypes(
-        this.provider,
-        this.keypair.getPublicKey().toSuiAddress(),
-        coinsSet
-      );
+        while (new Date().getTime() - startTime < duration) {
 
-      for (const address in this.pools) {
-        let pool = this.pools[address];
-        let price = await pool.estimatePrice();
+            for (const uri in this.dataSources) {
+                let dataSource = this.dataSources[uri];
+                let data = await dataSource.getData();
+                logger.info({
+                    price: data,
+                }, 'price')
 
-        let data: DataEntry = {
-          pool: pool,
-          priceOfB: price,
-          timestamp: new Date().getTime(),
-        };
+                // Push new data to all strategies subscribed to this data source
+                for (const strategy of this.strategies[uri]) {
 
-        logger.info(
-          {
-            pool: pool.pool,
-            price: price,
-          },
-          "price"
-        );
+                    // Get orders for this strategy.
+                    let tradeOrders = strategy.evaluate(data);
 
-        // Push new data to all strategies subscribed to this pool
-        for (const strategy of this.strategies[address]) {
-          // Compute a trading decision for this strategy.
-          let tradeSuggestions = strategy.evaluate(data);
-
-          let transactionBlock: TransactionBlock = new TransactionBlock();
-          transactionBlock.setGasBudget(1500000000);
-
-          // Execute any suggested trades
-          for (const tradeSuggestion of tradeSuggestions) {
-            // logger.info({ strategy: strategy.name, decision: tradeSuggestion });
-            let amountIn: number =
-              tradeSuggestion.amount *
-              defaultAmount[
-                tradeSuggestion.a2b ? pool.coinTypeA : pool.coinTypeB
-              ];
-            let amountOut: number = tradeSuggestion.estimatedPrice * amountIn;
-
-            const byAmountIn: boolean = true;
-            const slippage: number = 5; // Allow for 5% slippage (??)
-
-            const txb = await this.pools[
-              tradeSuggestion.pool
-            ].createSwapTransaction({
-              transactionBlock,
-              amountIn,
-              amountOut,
-              byAmountIn,
-              slippage,
-            });
-
-            if (typeof txb !== "undefined") {
-              transactionBlock = txb;
-              await this.signer
-                .signAndExecuteTransactionBlock({
-                  transactionBlock: transactionBlock,
-                  requestType: "WaitForLocalExecution",
-                  options: {
-                    showObjectChanges: true,
-                    showEffects: true,
-                  },
-                })
-                .then((result) => {
-                  // TODO: Execute transaction
-                  logger.info(
-                    { strategy: strategy, transaction: result },
-                    "transaction"
-                  );
-                })
-                .catch((e) => {
-                  logger.error(e);
-                });
-
-              coinsBalances = await getBalancesForCoinTypes(
-                this.provider,
-                this.keypair.getPublicKey().toSuiAddress(),
-                coinsSet
-              );
-              transactionBlock = new TransactionBlock();
+                    // Execute any suggested trades
+                    for (const order of tradeOrders) {
+                        logger.info({strategy: strategy.name, decision: order}, 'order');
+                        let amountIn = order.amountIn;
+                        let expectedAmountOut = order.estimatedPrice * amountIn;
+                        // TODO: Do these as a programmable transaction
+                        this.pools[order.pool].createSwapTransaction(
+                            order.a2b,
+                            amountIn,
+                            expectedAmountOut,
+                            true,
+                            5, // Allow for 5% slippage (??)
+                        ).then((result) => {
+                            // TODO: Execute transaction
+                            logger.info({strategy: strategy, transaction: result}, 'transaction');
+                        }).catch((e) => {
+                            logger.error(e);
+                        });
+                    }
+                }
             }
-          }
+            await setTimeout(delay);
         }
-      }
-      await setTimeout(delay);
     }
-  }
 
-  /** Add a strategy to this bot. The pools it subscribes to must have been added first. */
-  addStrategy(strategy: Strategy) {
-    for (const pool of strategy.subscribes_to()) {
-      if (!this.pools.hasOwnProperty(pool)) {
-        throw new Error("Bot does not know the pool with address " + pool);
-      }
-      this.strategies[pool].push(strategy);
+    /** Add a strategy to this bot. The pools it subscribes to must have been added first. */
+    addStrategy(strategy: Strategy) {
+        for (const dataSource of strategy.subscribes_to()) {
+            if (!this.dataSources.hasOwnProperty(dataSource)) {
+                throw new Error('Bot does not know the dataSource with address ' + dataSource);
+            }
+            this.strategies[dataSource].push(strategy);
+        }
     }
-  }
 
-  /** Add a new pool for this bot to use for trading. */
-  addPool(pool: Pool<CetusParams | SuiswapParams | TurbosParams>) {
-    if (this.pools.hasOwnProperty(pool.pool)) {
-      throw new Error("Pool " + pool.pool + " has already been added.");
+    /** Add a new price data source for this bot to use */
+    addDataSource(dataSource: DataSource) {
+        if (this.dataSources.hasOwnProperty(dataSource.uri)) {
+            throw new Error('Data source ' + dataSource.uri + " has already been added.");
+        }
+        this.dataSources[dataSource.uri] = dataSource;
+        this.strategies[dataSource.uri] = [];
     }
-    this.pools[pool.pool] = pool;
-    this.strategies[pool.pool] = [];
-  }
+
+    /** Add a new pool for this bot to use for trading. */
+    addPool(pool: Pool) {
+        if (this.pools.hasOwnProperty(pool.uri)) {
+            throw new Error('Pool ' + pool.uri + " has already been added.");
+        }
+        this.pools[pool.uri] = pool;
+        this.addDataSource(pool);
+    }
+
 }
