03d26ad20e81c78aaa78b469ade450a2d4169f44
“George <george.digkas@mystenlabs.com>
2023-06-16 12:49:45 +0300

    add ccxt fetchTickers from binance

--- utils/ticker-price-test.ts
+++ utils/ticker-price-test.ts
@@ -1,7 +1,12 @@
 import { BTCtoUSDC, BTCtoUSDT } from "./binance-ticker-price";
+import {
+  binanceFetchTickers,
+  binanceFetchTickersAll,
+} from "./ccxt-fetchTickers";
 
 let BTCtoUSDTprice: string;
 let BTCtoUSDCprice: string;
+let map = new Map();
 
 const getBTCtoUSDTprice = async () => {
   BTCtoUSDTprice = await BTCtoUSDT();
@@ -11,10 +16,22 @@ const getBTCtoUSDCprice = async () => {
   BTCtoUSDCprice = await BTCtoUSDC();
 };
 
-getBTCtoUSDTprice().then((res) => {
+const getCCXTBinanceTickers = async () => {
+  map = await binanceFetchTickers(["BTC/USDT", "BTC/USDC"]);
+};
+
+const getCCXTBinanceTickersAll = async () => {
+  map = await binanceFetchTickersAll();
+};
+
+getBTCtoUSDCprice().then(() => {
+  console.log(BTCtoUSDCprice);
+});
+
+getBTCtoUSDTprice().then(() => {
   console.log(BTCtoUSDTprice);
 });
 
-getBTCtoUSDCprice().then((res) => {
-  console.log(BTCtoUSDCprice);
+getCCXTBinanceTickers().then(() => {
+  console.log(map);
 });
