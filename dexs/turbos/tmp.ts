8db76d15527fee84cb8e81b376bd806dc4d2030b
“George <george.digkas@mystenlabs.com>
2023-06-19 09:12:46 +0300

    clean up

--- dexs/turbos/tmp.ts
+++ dexs/turbos/tmp.ts
@@ -1,194 +1,6 @@
-import {
-  Connection,
-  JsonRpcProvider,
-  SUI_CLOCK_OBJECT_ID,
-  TransactionArgument,
-  TransactionBlock,
-} from "@mysten/sui.js";
 import BN from "bn.js";
 import Decimal from "decimal.js";
-import { keypair } from "../../index";
-import { buildInputCoinForAmount } from "../../utils/utils";
-import { turbosConfig } from "../dexsConfig";
-import { TurbosParams } from "../dexsParams";
-import { Pool, PreswapResult } from "../pool";
-import { MAX_TICK_INDEX, MIN_TICK_INDEX } from "./constants";
-import { mainnet } from "./mainnet_config";
-import { testnet } from "./testnet_config";
 
-const ONE_MINUTE = 60 * 1000;
-
-enum sdkEnv {
-  mainnet = "mainnet",
-  testnet = "testnet",
-}
-
-// Use testnet or mainnet.
-const currSdkEnv: sdkEnv = sdkEnv.mainnet;
-
-function buildSdkOptions() {
-  switch (currSdkEnv) {
-    case sdkEnv.mainnet:
-      return mainnet;
-    case sdkEnv.testnet:
-      return testnet;
-  }
-}
-
-export class TurbosPool extends Pool<TurbosParams> {
-  private package: string;
-  private module: string;
-  private senderAddress: string;
-  private versioned: string;
-  private coinTypeC: string;
-
-  constructor(
-    address: string,
-    coinTypeA: string,
-    coinTypeB: string,
-    coinTypeC: string
-  ) {
-    super(address, coinTypeA, coinTypeB);
-    this.coinTypeC = coinTypeC;
-    this.senderAddress = keypair.getPublicKey().toSuiAddress();
-
-    this.package = turbosConfig.contract.PackageId;
-    this.module = turbosConfig.contract.ModuleId;
-    this.versioned = turbosConfig.contract.Versioned;
-  }
-
-  async createSwapTransaction(
-    params: TurbosParams
-  ): Promise<TransactionBlock | undefined> {
-    const txb = await this.createTransactionBlock(
-      params.a2b,
-      params.amountIn,
-      params.amountSpecifiedIsInput,
-      params.slippage
-    );
-    return txb;
-  }
-
-  async createTransactionBlock(
-    a2b: boolean,
-    amountIn: number,
-    amountSpecifiedIsInput: boolean,
-    slippage: number
-  ): Promise<TransactionBlock | undefined> {
-    console.log(
-      `Swap: (${amountIn}) [${a2b ? this.coinTypeA : this.coinTypeB}], 
-       To: [${!a2b ? this.coinTypeA : this.coinTypeB}], 
-       pool: ${this.uri}`
-    );
-    const admin = process.env.ADMIN_ADDRESS;
-
-    let provider = new JsonRpcProvider(
-      new Connection({
-        fullnode: mainnet.fullRpcUrl,
-      })
-    );
-
-    const functionName = a2b ? "swap_a_b" : "swap_b_a";
-
-    const transactionBlock = new TransactionBlock();
-
-    const coins: TransactionArgument[] | undefined =
-      await buildInputCoinForAmount(
-        transactionBlock,
-        BigInt(amountIn),
-        a2b ? this.coinTypeA : this.coinTypeB,
-        admin!,
-        provider
-      );
-
-    if (typeof coins !== "undefined") {
-      transactionBlock.moveCall({
-        target: `${this.package}::${this.module}::${functionName}`,
-        arguments: [
-          transactionBlock.object(this.uri),
-          transactionBlock.makeMoveVec({
-            objects: coins,
-          }),
-          transactionBlock.pure(amountIn.toFixed(0), "u64"),
-          transactionBlock.pure(
-            amountOutWithSlippage(
-              amountIn,
-              slippage.toString(),
-              amountSpecifiedIsInput
-            ),
-            "u64"
-          ),
-          transactionBlock.pure(
-            tickIndexToSqrtPriceX64(
-              a2b ? MIN_TICK_INDEX : MAX_TICK_INDEX
-            ).toString(),
-            "u128"
-          ),
-          transactionBlock.pure(amountSpecifiedIsInput, "bool"),
-          transactionBlock.object(this.senderAddress),
-          transactionBlock.pure(Date.now() + ONE_MINUTE * 3, "u64"),
-          transactionBlock.object(SUI_CLOCK_OBJECT_ID),
-          transactionBlock.object(this.versioned),
-        ],
-        typeArguments: [this.coinTypeA, this.coinTypeB, this.coinTypeC],
-      });
-
-      return transactionBlock;
-    }
-    return undefined;
-  }
-
-  async preswap(
-    a2b: boolean,
-    amount: number,
-    byAmountIn: boolean
-  ): Promise<PreswapResult> {
-    return {
-      estimatedAmountIn: 0,
-      estimatedAmountOut: 0,
-      estimatedFeeAmount: 0,
-    };
-  }
-
-  async estimatePrice(): Promise<number> {
-    // let pool = await this.sdk.Pool.getPool(this.uri);
-    // // current_sqrt_price is stored in Q64 format on Cetus
-    // return pool.current_sqrt_price ** 2 / 2 ** 128;
-    return 0;
-  }
-}
-
-function sqrtPriceX64ToPrice(
-  sqrtPriceX64: BN,
-  decimalsA: number,
-  decimalsB: number
-): Decimal {
-  return new Decimal(sqrtPriceX64.toString())
-    .mul(Decimal.pow(2, -64))
-    .pow(2)
-    .mul(Decimal.pow(10, decimalsA - decimalsB));
-}
-
-function amountOutWithSlippage(
-  amountOut: Decimal.Value,
-  slippage: string,
-  amountSpecifiedIsInput: boolean
-) {
-  if (amountSpecifiedIsInput) {
-    const minus = new Decimal(100).minus(slippage).div(100);
-    return new Decimal(amountOut).mul(minus).toFixed(0);
-  }
-
-  const plus = new Decimal(100).plus(slippage).div(100);
-  return new Decimal(amountOut).mul(plus).toFixed(0);
-}
-function tickIndexToSqrtPriceX64(tickIndex: number): BN {
-  if (tickIndex > 0) {
-    return new BN(tickIndexToSqrtPricePositive(tickIndex));
-  } else {
-    return new BN(tickIndexToSqrtPriceNegative(tickIndex));
-  }
-}
 
 function tickIndexToSqrtPriceNegative(tickIndex: number) {
   let tick = Math.abs(tickIndex);
@@ -454,3 +266,19 @@ function signedShiftRight(n0: BN, shiftBy: number, bitWidth: number) {
   twoN0.imaskn(bitWidth - shiftBy + 1);
   return twoN0.fromTwos(bitWidth - shiftBy);
 }
+
+
+function tickIndexToSqrtPriceX64(tickIndex: number): BN {
+  if (tickIndex > 0) {
+    return new BN(tickIndexToSqrtPricePositive(tickIndex));
+  } else {
+    return new BN(tickIndexToSqrtPriceNegative(tickIndex));
+  }
+}
+
+console.log(
+  tickIndexToSqrtPriceX64(510533319164864290).toString()
+    .mul(Decimal.pow(2, -64))
+    .pow(2)
+    .mul(Decimal.pow(10, 0 - 0))
+);
