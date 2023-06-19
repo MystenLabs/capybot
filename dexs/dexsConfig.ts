fb3bebc7e724aa8c85ed249ae17661865f3a4dbf
“George <george.digkas@mystenlabs.com>
2023-06-19 09:08:42 +0300

    update dexs config

--- dexs/dexsConfig.ts
+++ dexs/dexsConfig.ts
@@ -5,7 +5,31 @@ export const cetusConfig = {
 };
 
 export const turbosConfig = {
-  package: "0x84d1ad43e95e9833670fcdb2f2d9fb7618fe1827e3908f2c2bb842f3dccb80af",
-  module: "swap_router",
-  versioned: "0xf1cf0e81048df168ebeb1b8030fad24b3e0b53ae827c25053fff0779c1445b6f",
+  contract: {
+    PackageId:
+      "0x84d1ad43e95e9833670fcdb2f2d9fb7618fe1827e3908f2c2bb842f3dccb80af",
+    ModuleId: "swap_router",
+    PackageIdOriginal:
+      "0x91bfbc386a41afcfd9b2533058d7e915a1d3829089cc268ff4333d54d6339ca1",
+    PoolConfig:
+      "0xc294552b2765353bcafa7c359cd28fd6bc237662e5db8f09877558d81669170c",
+    Positions:
+      "0xf5762ae5ae19a2016bb233c72d9a4b2cba5a302237a82724af66292ae43ae52d",
+    PoolFactoryAdminCap:
+      "0x4bd4c54c84158e4ddab02ba612a84fc4b02dff3cb2ee687db0e46b5ab2f8bc5b",
+    Versioned:
+      "0xf1cf0e81048df168ebeb1b8030fad24b3e0b53ae827c25053fff0779c1445b6f",
+    PoolTableId:
+      "0x08984ed8705f44b6403705dc248896e56ab7961447820ae29be935ce0d32198b",
+  },
+  fee: {
+    "100bps":
+      "0x18136d55969b970330e6991e0deaa713fca99f4e526152c10d5592a01d45f857",
+    "500bps":
+      "0xdfcd1043f54fef3aa649eccd4d0b2b09a3da6c87a8a62736467b686268869365",
+    "3000bps":
+      "0x6bc433c8604f58272155d0c65327ae0b316f63869e5de3846f70aef4625d8c46",
+    "10000bps":
+      "0xd30c41ec4b4d0eb9b30b0b898c53f81cd53a9eb0f75b5d3273584cd874b2d4c4",
+  },
 };
