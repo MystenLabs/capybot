823a85fb0358beac87da23d4e78aa8468d2f42b5
“George <george.digkas@mystenlabs.com>
2023-06-23 15:19:12 +0300

    add code comments to ccxt-fetchTickers

--- src/utils/ccxt-fetchTickers.js
+++ src/utils/ccxt-fetchTickers.js
@@ -7,7 +7,7 @@ const kraken = new ccxt.kraken({ enableRateLimit: true });
 const kucoin = new ccxt.kucoin({ enableRateLimit: true });
 const okx = new ccxt.okx({ enableRateLimit: true });
 
-// Exchange;
+// This function fetches tickers from an exchange
 const exchangeFetchTickers = async function (exchange, pairs) {
   const map = new Map();
 
@@ -18,30 +18,37 @@ const exchangeFetchTickers = async function (exchange, pairs) {
   return map;
 };
 
+// This function fetches tickers from Binance exchange
 export const binanceFetchTickers = async function (pairs) {
   return await exchangeFetchTickers(binance, pairs);
 };
 
+// This function fetches tickers from Bitget exchange
 export const bitgetFetchTickers = async function (pairs) {
   return await exchangeFetchTickers(bitget, pairs);
 };
 
+// This function fetches tickers from Coinbase Prime exchange
 export const coinbaseprimeFetchTickers = async function (pairs) {
   return await exchangeFetchTickers(coinbaseprime, pairs);
 };
 
+// This function fetches tickers from Kraken exchange
 export const krakenFetchTickers = async function (pairs) {
   return await exchangeFetchTickers(kraken, pairs);
 };
 
+// This function fetches tickers from Kucoin exchange
 export const kucoinFetchTickers = async function (pairs) {
   return await exchangeFetchTickers(kucoin, pairs);
 };
 
+// This function fetches tickers from Okx exchange
 export const okxFetchTickers = async function (pairs) {
   return await exchangeFetchTickers(okx, pairs);
 };
 
+// This function fetches all tickers from Binance exchange
 export const binanceFetchTickersAll = async function () {
   const map = new Map();
 
